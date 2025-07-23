73af360c5bc26a57b3f85a8733d06adf3e75d2fc
SyedTazim20 <smhossaintazim@gmail.com>
2025-07-23 19:51:35 +0600

    backend: data pass to firebase server

---
+++ lib/db_service/database.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  Future addPersonalTask(
+    Map<String, dynamic> userPersonalMap,
+    String id,
+  ) async {
+    return await FirebaseFirestore.instance
+        .collection("Personal")
+        .doc(id)
+        .set(userPersonalMap);
+  }
+
+  Future addCollegeTask(Map<String, dynamic> userPersonalMap, String id) async {
+    return await FirebaseFirestore.instance
+        .collection("College")
+        .doc(id)
+        .set(userPersonalMap);
+  }
+
+  Future addOfficeTask(Map<String, dynamic> userPersonalMap, String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Office")
+        .doc(id)
+        .set(userPersonalMap);
+  }
+}
