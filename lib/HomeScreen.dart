3b3f35d55b05c472468a279f9a4d5ab485fbb1f1
Mohammad Aqib Ahsan <130603483+MohammadAqibAhsan@users.noreply.github.com>
2025-08-22 14:28:00 +0600

    Update HomeScreen.dart

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -15,172 +15,42 @@ class _HomeScreenState extends State<HomeScreen> {
   String selectedCategory = "";
   List<String> categories = [];
   TextEditingController todoController = TextEditingController();
-
   Stream<QuerySnapshot>? todoStream;
 
   @override
   void initState() {
     super.initState();
-    final user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      dbService = DatabaseService(user.email!);
-      loadCategories();
-    }
+    final user = FirebaseAuth.instance.currentUser!;
+    dbService = DatabaseService(user.email!);
+    loadCategories();
   }
 
-  Future<void> loadCategories() async {
-    final cats = await dbService.getCategories();
-    if (!mounted) return; // Prevent setState after dispose
-
+  void loadCategories() async {
+    final snapshot = await dbService.getCategories();
     setState(() {
-      categories = cats;
+      categories = snapshot.docs.map((doc) => doc.id).toList();
       if (categories.isNotEmpty) {
-        selectedCategory = categories.first;
+        selectedCategory = categories[0];
         loadTasks();
-      } else {
-        selectedCategory = "";
-        todoStream = null;
       }
     });
   }
 
-  void loadTasks() {
-    if (selectedCategory.isEmpty) {
-      todoStream = null;
-    } else {
-      todoStream = dbService.getTasks(selectedCategory);
-    }
-    if (mounted) setState(() {});
-  }
-
-  Widget taskList() {
-    if (todoStream == null) {
-      return Expanded(child: Center(child: Text("No category selected.")));
-    }
-
-    return Expanded(
-      child: StreamBuilder<QuerySnapshot>(
-        stream: todoStream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData)
-            return Center(child: CircularProgressIndicator());
-
-          if (snapshot.data!.docs.isEmpty)
-            return Center(child: Text("No tasks found."));
-
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              var doc = snapshot.data!.docs[index];
-              return CheckboxListTile(
-                activeColor: const Color(0xFF003049),
-                title: Text(doc['work']),
-                value: doc['Yes'],
-                onChanged: (bool? newValue) async {
-                  if (newValue == true) {
-                    await dbService.tickTask(selectedCategory, doc.id);
-                    Future.delayed(Duration(seconds: 2), () async {
-                      if (mounted) {
-                        await dbService.removeTask(selectedCategory, doc.id);
-                      }
-                    });
-                  }
-                },
-                controlAffinity: ListTileControlAffinity.leading,
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-
-  Future<void> showAddTaskDialog() async {
-    todoController.clear();
-    await showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Add Task"),
-        backgroundColor: const Color(0xfffdf0d5),
-        content: TextField(
-          controller: todoController,
-          decoration: InputDecoration(hintText: "Enter the task"),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("Cancel", style: TextStyle(color: Color(0xffc1121f))),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              final task = todoController.text.trim();
-              if (task.isEmpty) return;
-
-              final taskMap = {
-                'work': task,
-                'timestamp': DateTime.now(),
-                'Yes': false,
-              };
-
-              await dbService.addTask(selectedCategory, taskMap);
-              if (!mounted) return;
-              Navigator.pop(context);
-            },
-            child: Text("Add", style: TextStyle(color: Color(0xff003049))),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> showAddCategoryDialog() async {
-    final categoryController = TextEditingController();
-
-    await showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text("Add Category"),
-        backgroundColor: const Color(0xfffdf0d5),
-        content: TextField(
-          controller: categoryController,
-          decoration: InputDecoration(hintText: "Category name"),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("Cancel", style: TextStyle(color: Color(0xffc1121f))),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              final catName = categoryController.text.trim();
-              if (catName.isEmpty) return;
-
-              await dbService.addCategory(catName);
-              await loadCategories();
-              if (!mounted) return;
-              Navigator.pop(context);
-            },
-            child: Text("Add", style: TextStyle(color: Color(0xff003049))),
-          ),
-        ],
-      ),
-    );
+  void deleteCategory(String categoryName) async {
+    await dbService.deleteCategory(categoryName);
+    loadCategories();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Daily TaskMate",
-          style: TextStyle(color: const Color(0xfffdf0d5)),
-        ),
-        backgroundColor: const Color(0xff669bbc),
+        title: Text("Daily TaskMate"),
         actions: [
           IconButton(
-            icon: Icon(Icons.logout, color: Color(0xFFfdf0d5)),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
             },
           ),
         ],
@@ -189,76 +59,167 @@ class _HomeScreenState extends State<HomeScreen> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            heroTag: 'addCategory',
-            backgroundColor: const Color(0xff780000),
-            onPressed: showAddCategoryDialog,
-            child: Icon(Icons.category, color: Color(0xFFfdf0d5)),
-            tooltip: "Add Category",
+            heroTag: "addTask",
+            onPressed: () {
+                            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text("Add Task"),
+                  content: TextField(
+                    controller: todoController,
+                    decoration: InputDecoration(hintText: "Enter task..."),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Cancel"),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        if (todoController.text.isNotEmpty &&
+                            selectedCategory.isNotEmpty) {
+                          await dbService.addTask(
+                            selectedCategory,
+                            todoController.text,
+                          );
+                          todoController.clear();
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text("Add"),
+                    ),
+                  ],
+                ),
+              );
+            },
+            child: Icon(Icons.add),
           ),
-          SizedBox(height: 12),
+          SizedBox(height: 10),
           FloatingActionButton(
-            heroTag: 'addTask',
-            backgroundColor: const Color(0xff003049),
-            onPressed: selectedCategory.isEmpty ? null : showAddTaskDialog,
-            child: Icon(Icons.add, color: Color(0xFFfdf0d5)),
-            tooltip: selectedCategory.isEmpty
-                ? "Add category first"
-                : "Add Task",
+            heroTag: "addCategory",
+            onPressed: () {
+                            TextEditingController categoryController =
+                  TextEditingController();
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text("Add Category"),
+                  content: TextField(
+                    controller: categoryController,
+                    decoration: InputDecoration(hintText: "Enter category..."),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("Cancel"),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        if (categoryController.text.isNotEmpty) {
+
+                          await dbService.addCategory(categoryController.text);
+                          Navigator.pop(context);
+                          loadCategories();
+                        }
+                      },
+                      child: Text("Add"),
+                    ),
+                  ],
+                ),
+              );
+            },
+            child: Icon(Icons.create_new_folder),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Good to see you!", style: TextStyle(fontSize: 40)),
-
-            SizedBox(height: 20),
-            categories.isEmpty
-                ? Center(child: Text("No categories found. Add some!"))
-                : SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: categories.map((cat) {
-                        final isSelected = cat == selectedCategory;
-                        return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              selectedCategory = cat;
-                              loadTasks();
-                            });
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 15,
-                              vertical: 8,
-                            ),
-                            margin: EdgeInsets.only(right: 8),
-                            decoration: BoxDecoration(
-                              color: isSelected
-                                  ? const Color(0xff780000)
-                                  : Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Text(
-                              cat,
-                              style: TextStyle(
-                                color: isSelected
-                                    ? const Color(0xfffdf0d5)
-                                    : const Color(0xff003049),
-                              ),
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-            SizedBox(height: 20),
-            taskList(),
-          ],
-        ),
+      body: Column(
+        children: [
+          categoryList(),
+          Expanded(child: taskList()),
+        ],
       ),
+    );
+  }
+
+    Widget categoryList() {
+    return SizedBox(
+      height: 60,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          final category = categories[index];
+          return GestureDetector(
+            onLongPress: () {
+
+              deleteCategory(category);
+            },
+            onTap: () {
+              setState(() {
+                selectedCategory = category;
+                loadTasks();
+              });
+            },
+            child: Container(
+              margin: EdgeInsets.all(8),
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              decoration: BoxDecoration(
+                color: selectedCategory == category
+                    ? Colors.blue
+                    : Colors.grey[300],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Text(
+                  category,
+                  style: TextStyle(
+                    color: selectedCategory == category
+                        ? Colors.white
+                        : Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+    Widget taskList() {
+    if (todoStream == null) return Center(child: Text("No category selected"));
+
+    return StreamBuilder(
+      stream: todoStream,
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData)
+          return Center(child: CircularProgressIndicator());
+
+        final tasks = snapshot.data!.docs;
+        return ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            final task = tasks[index];
+            return ListTile(
+              title: Text(task['title']),
+              trailing: Checkbox(
+                value: task['isDone'],
+                onChanged: (val) async {
+                  // ==== BACKEND WRITER 2 (Update Task Status/Delete) ====
+                  if (val == true) {
+                    await dbService.updateTask(selectedCategory, task.id, true);
+                    await dbService.deleteTask(selectedCategory, task.id);
+                  }
+                },
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
